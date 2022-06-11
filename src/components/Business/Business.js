d56df74be5a297c7e5eabb4de4c4383b41643be7
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-11 13:30:14 +1000

    created Business List component and added scaffold code. imported backgrounds. created search bar component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Business.css";
 
 let business = {
 	imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
@@ -17,17 +18,16 @@ class Business extends React.Component {
 		return (
 			<div className="Business">
 				<div className="image-container">
-					<img
-						src={business.imageSrc}
-						alt={business.name}
-					/>
+					<img src={business.imageSrc} alt={business.name} />
 				</div>
 				<h2>{business.name}</h2>
 				<div className="Business-information">
 					<div className="Business-address">
 						<p>{business.address}</p>
 						<p>{business.city}</p>
-						<p>{business.state} {business.zipCode}</p>
+						<p>
+							{business.state} {business.zipCode}
+						</p>
 					</div>
 					<div className="Business-reviews">
 						<h3>{business.category}</h3>
@@ -39,3 +39,5 @@ class Business extends React.Component {
 		);
 	}
 }
+
+export default Business;
