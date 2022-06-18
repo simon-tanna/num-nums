39e68c00d681c78f4d477bb366da82a387865ca8
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-18 14:46:32 +1000

    created class methods to handle change in sorting options in search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,25 +1,36 @@
 import React from "react";
 import "./SearchBar.css";
 
-const sortByOptions = {
-	"Best Match": "best_match",
-	"Highest Reviewed": "rating",
-	"Most Reviewed": "review_count",
-};
-
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			term: "",
 			location: "",
-			soryBy: "best_match",
+			sortBy: "best_match",
+		};
+		this.sortByOptions = {
+			"Best Match": "best_match",
+			"Highest Reviewed": "rating",
+			"Most Reviewed": "review_count",
 		};
 	}
 
+	getSortByClass(sortByOption) {
+		if (this.state.sortBy === sortByOption) {
+			return "active";
+		} else {
+			return "";
+		}
+	}
+
+	handleSortByChange(sortByOption) {
+		this.setState({ sortBy: sortByOption });
+	}
+
 	renderSortByOptions() {
-		return Object.keys(sortByOptions).map((sortByOption) => {
-			let sortByOptionValue = sortByOptions[sortByOption];
+		return Object.keys(this.sortByOptions).map((sortByOption) => {
+			let sortByOptionValue = this.sortByOptions[sortByOption];
 			return <li key={sortByOptionValue}>{sortByOption}</li>;
 		});
 	}
