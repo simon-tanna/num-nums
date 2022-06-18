f5e653e046f503794f3f974aa260dce5a2e1b28d
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-18 14:56:13 +1000

    created event handlers for change in business and location

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -28,6 +28,14 @@ class SearchBar extends React.Component {
 		this.setState({ sortBy: sortByOption });
 	}
 
+	handleTermChange(event) {
+		this.setState({ term: event.target.value });
+	}
+
+	handleLocationChange(event) {
+		this.setState({ location: event.target.value });
+	}
+
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map((sortByOption) => {
 			let sortByOptionValue = this.sortByOptions[sortByOption];
